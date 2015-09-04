e1612c903c31d106b208ff3984c41ee3a5bc2e99
Mike Bronner <mike@genealabs.com>
2015-09-04 13:27:11 -0700

    use in-memory database

--- config/database.php
+++ config/database.php
@@ -48,7 +48,8 @@ return [
 
         'sqlite' => [
             'driver'   => 'sqlite',
-            'database' => storage_path('database.sqlite'),
+            'database' => ':memory:',
+//            'database' => storage_path('database.sqlite'),
             'prefix'   => '',
         ],
 
