434fe1cb642f79e9d018f907978aa7c6119d0aed
Mike Bronner <mike@genealabs.com>
2015-09-06 11:15:09 -0700

    Add provisional entity edit tests

---
+++ tests/resources/views/entities/EntitiesEditViewTest.php
@@ -0,0 +1,57 @@
+<?php
+
+class EntitiesEditViewTest extends TestCase
+{
+    /** @test */
+    public function it_returns_403_for_user_without_roles()
+    {
+        $this->prepare();
+
+        $response = $this->actingAs($this->unauthorizedUser)
+            ->call('GET', 'genealabs/laravel-governor/entities/entity/edit');
+
+        $this->assertEquals(403, $response->getStatusCode());
+    }
+
+    /** @test */
+    public function it_shows_rules_page_for_user_with_superadmin_role()
+    {
+        $this->prepare();
+
+        $response = $this->actingAs($this->superAdminUser)
+            ->visit('genealabs/laravel-governor/entities/entity/edit')
+            ->see('Roles');
+    }
+
+    /** @test */
+//    public function it_edits_role_for_user_with_superadmin_role()
+//    {
+//        $this->prepare();
+//
+//        $response = $this->actingAs($this->superAdminUser)
+//            ->visit('genealabs/laravel-governor/entities/entity/edit')
+//            ->click('selected-entity-create')
+//            ->click('create any')
+//            ->press('Update Role');
+//
+//        dd($response);
+//    }
+
+    /** @test */
+//    public function it_can_delete_a_role_as_a_superadmin_user()
+//    {
+//        $this->prepare();
+//
+//        $response = $this->actingAs($this->superAdminUser)
+//            ->visit('genealabs/laravel-governor/entities/create')
+//            ->type('TestRole', 'name')
+//            ->type('This is a description for test users role.', 'description')
+//            ->press('Add Role')
+//            ->see('TestRole')
+//            ->onPage('genealabs/laravel-governor/roles');
+//
+//        $response = $this->actingAs($this->superAdminUser)
+//            ->visit('genealabs/laravel-governor/roles/TestRole/edit')
+//            ->press('Delete Role');
+//    }
+}
