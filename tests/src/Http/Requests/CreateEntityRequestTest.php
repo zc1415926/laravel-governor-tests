0a83cfabead89623ec13303e7d6ca67f47c2e24f
Mike Bronner <mike@genealabs.com>
2015-09-06 18:36:29 -0700

    Add unit tests for request models

---
+++ tests/src/Http/Requests/CreateEntityRequestTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use GeneaLabs\LaravelGovernor\Http\Requests\CreateEntityRequest;
+
+class CreateEntityRequestTest extends TestCase
+{
+    /** @test */
+    public function it_is_authorized()
+    {
+        $this->prepare();
+
+        $request = new CreateEntityRequest();
+        $this->be($this->superAdminUser);
+
+        $this->assertTrue($request->authorize());
+        $this->be($this->unauthorizedUser);
+        $this->assertFalse($request->authorize());
+    }
+
+    /** @test */
+    public function it_has_the_right_rules()
+    {
+        $this->prepare();
+
+        $request = new CreateEntityRequest();
+
+        $this->assertTrue(array_key_exists('name', $request->rules()));
+        $this->assertEquals('required', $request->rules()['name']);
+    }
+}
---
+++ tests/src/Http/Requests/UpdateRoleRequestTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use GeneaLabs\LaravelGovernor\Http\Requests\CreateRoleRequest;
+
+class UpdateRoleRequestTest extends TestCase
+{
+    /** @test */
+    public function it_is_authorized()
+    {
+        $this->prepare();
+
+        $request = new CreateRoleRequest();
+        $this->be($this->superAdminUser);
+
+        $this->assertTrue($request->authorize());
+        $this->be($this->unauthorizedUser);
+        $this->assertFalse($request->authorize());
+    }
+
+    /** @test */
+    public function it_has_the_right_rules()
+    {
+        $this->prepare();
+
+        $request = new CreateRoleRequest();
+
+        $this->assertTrue(array_key_exists('name', $request->rules()));
+        $this->assertEquals('required', $request->rules()['name']);
+    }
+}
